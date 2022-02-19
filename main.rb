51039724908a10c6364883a8aef170aedb26757e
CztCFfSNI <jerrychoiczt@gmail.com>
2022-02-19 14:22:50 -0500

    do a test for coursicle

--- main.rb
+++ main.rb
@@ -20,17 +20,26 @@ print "Enter anything else to quit the program\n"
 print "Choice: "
 choice = gets.chomp
 
-    if choice == "1"
-        # create a scraper to scrape osu websire, put a course identifier into the course scraper, return an array of all sections of cnum.
-        osuScraper = Scraper.new OSU_SEARCH_URL
-        instructorList = osuScraper.getInstructorsOSU cnum  
-    elsif choice == "2"
-        # create a scraper to scrape coursicle.com, put a course identifier into the course scraper, return an array of all sections of cnum.
-        coursicleScraper = Scraper.new COURSICLE_SEARCH_URL
-        instructorList = coursicleScraper.getInstructorsCoursicle cnum
-    else abort("Goodbye.\n")
+if choice == '1'
+    # create a scraper to scrape osu websire, put a course identifier into the course scraper, return an array of all sections of cnum.
+    osuScraper = Scraper.new OSU_SEARCH_URL
+    instructorList = osuScraper.getInstructorsOSU cnum  
+elsif choice == '2'
+    # create a scraper to scrape coursicle.com, put a course identifier into the course scraper, return an array of all sections of cnum.
+    coursicleScraper = Scraper.new COURSICLE_SEARCH_URL
+    instructorList = coursicleScraper.getInstructorsCoursicle cnum
+    if instructorList == [] 
+        print "You're not able to use COURSICLE SEARCH, enter 1 if you want to switch to OSU SEARCH: "
+        choice = gets.chomp
+        puts ""
+        if choice == '1'
+            osuScraper = Scraper.new OSU_SEARCH_URL
+            instructorList = osuScraper.getInstructorsOSU cnum 
+        else abort "Goodbye.\n"
+        end
     end
-
+else abort "Goodbye.\n"
+end
 
 # create a scraper to scrape ratemyprofessor.com and find the ratings of each professor in instructorList
 rmpScraper = Scraper.new RMP_URL
